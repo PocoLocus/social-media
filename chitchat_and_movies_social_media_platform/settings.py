752ce1e4ec2499c59ad12fd0e8ab7343bfdc6210
poco <poco@email.com>
2025-06-12 13:30:15 +0300

    Updated .env

--- chitchat_and_movies_social_media_platform/settings.py
+++ chitchat_and_movies_social_media_platform/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "pythonpoco2024@gmail.com"
-EMAIL_HOST_PASSWORD = "bacd vxec jlfm hyab"
+EMAIL_HOST_USER = os.getenv("SENDER_EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
